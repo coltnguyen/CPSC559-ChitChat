82738f4537d1baf74263d09db92f4c6acb6bae17
Colton Nguyen <advancewolf@gmail.com>
2024-02-20 19:15:53 -0700

    Connected MongoDB to Djago Project

--- ChitChat/ChitChat/settings.py
+++ ChitChat/ChitChat/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ChitChat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'chitchat_db',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+           'host': 'mongodb+srv://coltvnguyen:Legitpassword12@chitchat-cluster-1.gjvrsgz.mongodb.net/?retryWrites=true&w=majority'
+        }
     }
 }
 
