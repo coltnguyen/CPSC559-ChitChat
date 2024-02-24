76a645a41a9475720afb101e06b86bbaf0a631ff
Makeda Morris <64229732+mkaeda@users.noreply.github.com>
2024-02-23 23:17:19 -0700

    update admin.py models

--- ChitChat/chat/admin.py
+++ ChitChat/chat/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
+from .models import User, ChatRoom, Message
 
-# Register your models here.
-from .models import User
+# We should probably have distinct admin models here but that's for later.
+
 admin.site.register(User)
+admin.site.register(ChatRoom)
+admin.site.register(Message)
