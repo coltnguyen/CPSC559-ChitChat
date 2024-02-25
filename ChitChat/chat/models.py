8603b3a21bf41f5d27515954174b304d7a982c97
Colton Nguyen <advancewolf@gmail.com>
2024-02-24 20:41:23 -0700

    Small code comments

--- ChitChat/chat/models.py
+++ ChitChat/chat/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
-# Create your models here.
 from django.contrib.auth.hashers import make_password
 
 class User(models.Model):
-    firstName = models.CharField(max_length=50)
-    lastName = models.CharField(max_length=50)
-    userName = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=50)
+    # Define the fields for the User model
+    firstName = models.CharField(max_length=50)  # User's first name
+    lastName = models.CharField(max_length=50)  # User's last name
+    userName = models.CharField(max_length=50, unique=True)  # User's username, must be unique
+    password = models.CharField(max_length=50)  # User's password
 
     def set_password(self, raw_password):
-        self.password = raw_password
-        self.save()
+        # Set the user's password and save the user instance
+        self.password = raw_password  # Assign the raw password to the user's password field
+        self.save()  # Save the user instance to the database
