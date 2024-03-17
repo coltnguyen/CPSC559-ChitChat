ee8fa7dd0cc4d1fb2893bf334e82c5ad2391277b
Makeda Morris <64229732+mkaeda@users.noreply.github.com>
2024-03-16 18:01:21 -0600

    fix previous msg display

--- ChitChat/chat/views.py
+++ ChitChat/chat/views.py
@@ -62,7 +62,7 @@ def loginUser(request):
 
     # Manually check the username and password against the database
     try:
-        user = User.objects.get(userName=username, password=password)
+        _ = User.objects.get(userName=username, password=password)
         # If the user is found, return a success response
         return JsonResponse({"username": username, "chatroom": "global"}, status=status.HTTP_200_OK)
     except User.DoesNotExist:
