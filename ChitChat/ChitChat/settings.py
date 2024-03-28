5923d1803c5c3fe52acc457866100da0335a4b17
jaepark117 <99509378+jaepark117@users.noreply.github.com>
2024-03-27 23:38:21 -0600

    Add files via upload

--- ChitChat/ChitChat/settings.py
+++ ChitChat/ChitChat/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 import os
@@ -138,7 +139,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+CELERY_BEAT_SCHEDULE = {
+    'check_default_database': {
+        'task': 'chat.tasks.run_leader_election',
+        'schedule': timedelta(secondes=10),
+    }
+}
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
