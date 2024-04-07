6f4fdb43499f4e26d6b5819f619ca0413a2c775e
Colton Nguyen <advancewolf@gmail.com>
2024-04-06 23:38:10 -0600

    Implemented a task completion check for Heartbeat

--- ChitChat/chat/tasks.py
+++ ChitChat/chat/tasks.py
@@ -7,41 +7,40 @@ import time
 from .management.commands.sync_databases import run_synchronization
 
 logger = get_task_logger(__name__)
-redis_client = Redis(host='192.168.50.152', port=6379, db=0)
+redis_client = Redis(host='10.13.151.7', port=6379, db=0)
 
-# Create a Redlock instance with a single Redis server
 redlock = Redlock([
-    {'host': '192.168.50.152', 'port': 6379, 'db': 0}
+    {'host': '10.13.151.7', 'port': 6379, 'db': 0}
 ])
 
 @shared_task(
-    bind=True,  # This binds the first argument of the function to the task instance (self)
-    max_retries=3,  # Maximum number of retries before giving up
-    retry_backoff=True,  # Enables exponential backoff delay, doubling the retry delay for each retry
-    retry_backoff_max=60  # Maximum number of seconds the retry delay can reach
+    bind=True,
+    max_retries=3,
+    retry_backoff=True,
+    retry_backoff_max=60
 )
 def run_sync_databases(self):
     lock_id = 'sync_databases_lock'
     lock_ttl = 300000  # Lock TTL in milliseconds (5 minutes)
     have_lock = False
+    sync_completed = threading.Event()
 
     try:
-        # Acquire the lock using Redlock
         lock = redlock.lock(lock_id, lock_ttl)
         if lock:
             have_lock = True
             logger.info('Acquired lock. Running synchronization...')
 
-            # Start a separate thread to periodically extend the lock's TTL
             def heartbeat():
-                while have_lock:
-                    redlock.extend(lock, additional_time=45000)  # Extend the lock by 45 seconds minute (milliseconds)
+                while not sync_completed.is_set():
+                    redlock.extend(lock, additional_time=45000)  # Extend the lock by 45 seconds (in milliseconds)
                     time.sleep(30)  # Sleep for 30 seconds before the next heartbeat
 
             heartbeat_thread = threading.Thread(target=heartbeat)
             heartbeat_thread.start()
 
             run_synchronization()
+            sync_completed.set()  # Signal that synchronization is completed
             logger.info('Synchronization completed.')
         else:
             logger.info('Failed to acquire lock. Skipping synchronization.')
@@ -51,5 +50,6 @@ def run_sync_databases(self):
     finally:
         if have_lock:
             redlock.unlock(lock)
-            have_lock = False  # Set the flag to stop the heartbeat thread
+            have_lock = False
+            sync_completed.set()  # Signal that synchronization is completed (in case of an exception)
             heartbeat_thread.join()  # Wait for the heartbeat thread to finish
